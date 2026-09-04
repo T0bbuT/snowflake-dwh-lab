da11aaf4b8b6db88534c05aa8507169764b32ac6
john <65542008+T0bbuT@users.noreply.github.com>
2026-09-04 17:12:39 +0900

    feat: gold層のddlスクリプト完成

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -42,16 +42,17 @@ WHERE
 ;
 
 -- fact_sales
+CREATE OR REPLACE VIEW data_warehouse.gold.fact_sales AS
 SELECT
-    sd.sls_ord_num,
+    sd.sls_ord_num  AS order_number,
     pr.product_key,
     cu.customer_key,
-    sd.sls_order_dt,
-    sd.sls_ship_dt,
-    sd.sls_due_dt,
-    sd.sls_sales,
-    sd.sls_quantity,
-    sd.sls_price,
+    sd.sls_order_dt AS order_date,
+    sd.sls_ship_dt  AS shipping_date,
+    sd.sls_due_dt   AS due_date,
+    sd.sls_sales    AS sales_amount,
+    sd.sls_quantity AS quantity,
+    sd.sls_price    AS price,
 FROM
     data_warehouse.silver.crm_sales_details AS sd
     LEFT JOIN data_warehouse.gold.dim_products AS pr ON sd.sls_prd_key = pr.product_number
